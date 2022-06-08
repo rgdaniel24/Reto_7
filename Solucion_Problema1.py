10f6d99243b40898c5ca3525b6979357297a8c90
rgdaniel24 <rgdaniel26@gmail.com>
2022-06-08 14:45:34 -0500

    Actualización del reto_7

--- Solucion_Problema1.py
+++ Solucion_Problema1.py
@@ -2,7 +2,5 @@ import pandas as dp
 
 ventas = {'Mes': ['Enero', 'febrero', 'Marzo', 'Abril'], 'Ventas': [30500, 35600, 28300, 33900]}
 
-df = DataFrame(ventas)
+df = dp.DataFrame(ventas)
 print(df)
-
-
